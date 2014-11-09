5616d7914ee9952c8529cd92a74d7f0b16bae656
Tim Brooks <tim.brooks@staples.com>
2014-11-09 14:53:11 -0800

    Add initial clojure api.

--- src/fault/core.clj
+++ src/fault/core.clj
@@ -1,2 +1,10 @@
-(ns fault.core)
+(ns fault.core
+  (:import (fault.java ServiceExecutor ResilientAction)))
 
+
+(defn service-executor [num-of-threads]
+  (ServiceExecutor. num-of-threads))
+
+(defn resilient-action [f]
+  (reify ResilientAction
+    (run [_] (f))))
