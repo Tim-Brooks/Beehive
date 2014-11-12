f81c576a526209165894d24bd6879a4e2d3694c2
Tim Brooks <tim.brooks@staples.com>
2014-11-11 22:06:30 -0800

    WIP: Started implementing queue for exchanging tasks

---
+++ src/fault/java/concurrent/ExchangingQueue.java
@@ -0,0 +1,18 @@
+package fault.java.concurrent;
+
+import java.util.Queue;
+
+/**
+ * Created by timbrooks on 11/11/14.
+ */
+public class ExchangingQueue<E> {
+    
+    public boolean offer(E element) {
+        return true;
+    }
+
+    public E poll() {
+        return null;
+    }
+
+}
