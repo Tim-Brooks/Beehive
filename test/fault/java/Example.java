5d5374249194dbdf83c1ede04a2e48d4df2d6af8
Tim Brooks <tim.brooks@staples.com>
2014-11-17 22:07:59 -0800

    Change example to test some new scenarios and compare against future

--- test/fault/java/Example.java
+++ test/fault/java/Example.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.concurrent.Executors;
 
 /**
  * Created by timbrooks on 11/6/14.
@@ -17,10 +18,15 @@ public class Example {
         SingleWriterServiceExecutor serviceExecutor = new SingleWriterServiceExecutor(10);
 
         for (int i = 0; i < 50; ++i) {
-            new Thread(new ExampleRequest(serviceExecutor)).start();
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            new Thread(new ExampleRequest(serviceExecutor, Executors.newFixedThreadPool(20))).start();
         }
         try {
-            Thread.sleep(3000);
+            Thread.sleep(8000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- test/fault/java/ExampleRequest.java
+++ test/fault/java/ExampleRequest.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.concurrent.*;
 
 /**
  * Created by timbrooks on 11/16/14.
@@ -14,36 +15,51 @@ import java.net.URL;
 public class ExampleRequest implements Runnable {
 
     private final SingleWriterServiceExecutor serviceExecutor;
+    private final ExecutorService executorService;
 
-    public ExampleRequest(SingleWriterServiceExecutor serviceExecutor) {
+    public ExampleRequest(SingleWriterServiceExecutor serviceExecutor, ExecutorService executorService) {
         this.serviceExecutor = serviceExecutor;
+        this.executorService = executorService;
     }
 
     public void run() {
         ResilientPromise<String> result = serviceExecutor.performAction(new ResilientAction<String>() {
             @Override
             public String run() throws Exception {
+                Thread.sleep(10000);
                 String result = null;
                 InputStream response = new URL("http://localhost:6001/").openStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(response));
                 result = reader.readLine();
                 return result;
             }
-        }, 5);
+        }, 805);
+
+//        Future<Object> result = executorService.submit(new Callable<Object>() {
+//            @Override
+//            public Object call() throws Exception {
+//                String result = null;
+//                InputStream response = new URL("http://localhost:6001/").openStream();
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(response));
+//                result = reader.readLine();
+//                return result;
+//            }
+//        });
 
         long start = System.currentTimeMillis();
         try {
             result.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+//            result.get(10L, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+//            e.printStackTrace();
         }
 
         long end = System.currentTimeMillis();
-        System.out.println(end - start);
-        System.out.println("Result: " + result.result);
-        if (result.isError()) {
-            System.out.println(result.error.getMessage());
-        }
-        System.out.println("Is Done " + result.isDone());
+//        System.out.println(end - start);
+//        System.out.println("Result: " + result.result);
+//        if (result.isError()) {
+//            System.out.println(result.error.getMessage());
+//        }
+//        System.out.println("Is Done " + result.isDone());
     }
 }
