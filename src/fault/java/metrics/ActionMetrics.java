0bb4ea29fdada4c796bdf54fd3f94343c41b5bb2
Tim Brooks <tim.brooks@staples.com>
2014-11-23 20:23:03 -0500

    Implement error tracking with ActionMetrics. Add test

--- src/fault/java/metrics/ActionMetrics.java
+++ src/fault/java/metrics/ActionMetrics.java
@@ -8,44 +8,69 @@ import fault.java.Status;
 public class ActionMetrics implements IActionMetrics {
 
     private final int[] errorMetrics;
+    private final int[] successMetrics;
+    private final int[] timeoutMetrics;
     private long advanceSlotTimeInMillis;
     private int slotNumber;
+    private int totalSlots;
 
     public ActionMetrics() {
-        this.errorMetrics = new int[1000];
+        this.totalSlots = 1000;
+        this.errorMetrics = new int[totalSlots];
+        this.successMetrics = new int[totalSlots];
+        this.timeoutMetrics = new int[totalSlots];
         this.slotNumber = 0;
         this.advanceSlotTimeInMillis = System.currentTimeMillis() + 1000;
     }
 
     @Override
     public int getFailuresForTimePeriod(int milliseconds) {
-//        long currentTimestamp = System.currentTimeMillis();
-//        int slotsToAdvance = slotNumber + (int) ((advanceSlotTimeInMillis - currentTimestamp) / 1000);
-//        this.slotNumber = slotsToAdvance;
-//        this.advanceSlotTimeInMillis = advanceSlotTimeInMillis + (1000 * slotsToAdvance) + 1000;
-//
-//        int slotsBack = milliseconds / 1000;
-//        int totalErrors = 0;
-//        for (int i = slotNumber - slotsBack; i <= slotNumber; ++i) {
-//            totalErrors = totalErrors + errorMetrics[i];
-//        }
-//
-//        return totalErrors;
-        return 0;
+        long currentTimestamp = System.currentTimeMillis();
+        advanceToCurrentSlot(currentTimestamp);
+
+        int slotsBack = milliseconds / 1000;
+        int totalErrors = 0;
+        for (int i = slotNumber - slotsBack; i <= slotNumber; ++i) {
+            if (i < 0) {
+                totalErrors = totalErrors + errorMetrics[totalSlots + i] + timeoutMetrics[totalSlots + i];
+            } else {
+                totalErrors = totalErrors + errorMetrics[i] + timeoutMetrics[i];
+            }
+        }
+
+        return totalErrors;
     }
 
     @Override
     public void reportActionResult(Status status) {
-        if (status == Status.SUCCESS) {
-            long currentTimestamp = System.currentTimeMillis();
-            if (currentTimestamp < advanceSlotTimeInMillis) {
-                errorMetrics[slotNumber]++;
-            } else {
-                int slotsToAdvance = slotNumber + (int) ((advanceSlotTimeInMillis - currentTimestamp) / 1000);
-                this.slotNumber = slotsToAdvance;
-                this.advanceSlotTimeInMillis = advanceSlotTimeInMillis + (1000 * slotsToAdvance) + 1000;
-            }
+        long currentTimestamp = System.currentTimeMillis();
+        int[] metrics = this.errorMetrics;
+        switch (status) {
+            case SUCCESS:
+                metrics = this.successMetrics;
+                break;
+            case ERROR:
+                metrics = this.errorMetrics;
+                break;
+            case TIMED_OUT:
+                metrics = this.timeoutMetrics;
+                break;
         }
-
+        if (currentTimestamp < advanceSlotTimeInMillis) {
+            metrics[slotNumber]++;
+        } else {
+            advanceToCurrentSlot(currentTimestamp);
+        }
     }
+
+    private void advanceToCurrentSlot(long currentTimestamp) {
+        int slotsToAdvance =  1 + (int) ((advanceSlotTimeInMillis - currentTimestamp) / 1000);
+        int newSlotNumber = slotsToAdvance + slotNumber;
+        if (newSlotNumber >= totalSlots) {
+            newSlotNumber = newSlotNumber - totalSlots;
+        }
+        this.slotNumber = newSlotNumber;
+        this.advanceSlotTimeInMillis = advanceSlotTimeInMillis + (1000 * slotsToAdvance);
+    }
+
 }
--- test/fault/java/metrics/ActionMetricsTest.java
+++ test/fault/java/metrics/ActionMetricsTest.java
@@ -1,7 +1,10 @@
 package fault.java.metrics;
 
+import fault.java.Status;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,5 +23,15 @@ public class ActionMetricsTest {
     @Test
     public void testMetricsReportsNoErrorsIfNoErrorsOrTimeouts() {
         assertEquals(0, actionMetrics.getFailuresForTimePeriod(1000));
+    }
+
+    @Test
+    public void testMetricsReportCorrectErrorCount() {
+        int errorCount = new Random().nextInt(100);
+        for (int i = 0; i < errorCount; ++i) {
+            actionMetrics.reportActionResult(Status.ERROR);
+        }
+        assertEquals(errorCount, actionMetrics.getFailuresForTimePeriod(10000));
+
     }
 }
