c1431b52c5d8283b13b3d48ba8aee8c9f575f024
Tim Brooks <tim.brooks@staples.com>
2014-11-24 10:34:20 -0600

    use atomic longs so that ActionMetrics can be read by another thread

--- src/fault/java/metrics/ActionMetrics.java
+++ src/fault/java/metrics/ActionMetrics.java
@@ -3,18 +3,22 @@ package fault.java.metrics;
 import fault.java.Status;
 import fault.java.utils.TimeProvider;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by timbrooks on 11/10/14.
  */
 public class ActionMetrics implements IActionMetrics {
 
-    private final int[] errorMetrics;
-    private final int[] successMetrics;
-    private final int[] timeoutMetrics;
+    private final AtomicIntegerArray errorMetrics;
+    private final AtomicIntegerArray successMetrics;
+    private final AtomicIntegerArray timeoutMetrics;
     private final TimeProvider timeProvider;
-    private long advanceSlotTimeInMillis;
-    private int slotNumber;
-    private int totalSlots;
+    private final int totalSlots;
+    private final AtomicLong advanceSlotTimeInMillis;
+    private final AtomicInteger slotNumber;
 
     public ActionMetrics() {
         this(new TimeProvider());
@@ -23,27 +27,28 @@ public class ActionMetrics implements IActionMetrics {
     public ActionMetrics(TimeProvider timeProvider) {
         this.timeProvider = timeProvider;
         this.totalSlots = 1000;
-        this.errorMetrics = new int[totalSlots];
-        this.successMetrics = new int[totalSlots];
-        this.timeoutMetrics = new int[totalSlots];
-        this.slotNumber = 0;
-        this.advanceSlotTimeInMillis = timeProvider.currentTimeMillis() + 1000L;
+        this.errorMetrics = new AtomicIntegerArray(totalSlots);
+        this.successMetrics = new AtomicIntegerArray(totalSlots);
+        this.timeoutMetrics = new AtomicIntegerArray(totalSlots);
+        this.slotNumber = new AtomicInteger(0);
+        this.advanceSlotTimeInMillis = new AtomicLong(timeProvider.currentTimeMillis() + 1000L);
     }
 
     @Override
     public int getFailuresForTimePeriod(int milliseconds) {
         long currentTimestamp = timeProvider.currentTimeMillis();
-        if (currentTimestamp >= advanceSlotTimeInMillis) {
+        if (currentTimestamp >= advanceSlotTimeInMillis.get()) {
             advanceToCurrentSlot(currentTimestamp);
         }
 
         int slotsBack = milliseconds / 1000;
         int totalErrors = 0;
+        int slotNumber = this.slotNumber.get();
         for (int i = slotNumber - slotsBack; i <= slotNumber; ++i) {
             if (i < 0) {
-                totalErrors = totalErrors + errorMetrics[totalSlots + i] + timeoutMetrics[totalSlots + i];
+                totalErrors = totalErrors + errorMetrics.get(totalSlots + i) + timeoutMetrics.get(totalSlots + i);
             } else {
-                totalErrors = totalErrors + errorMetrics[i] + timeoutMetrics[i];
+                totalErrors = totalErrors + errorMetrics.get(i) + timeoutMetrics.get(i);
             }
         }
 
@@ -53,7 +58,7 @@ public class ActionMetrics implements IActionMetrics {
     @Override
     public void reportActionResult(Status status) {
         long currentTimestamp = timeProvider.currentTimeMillis();
-        int[] metrics = this.errorMetrics;
+        AtomicIntegerArray metrics = this.errorMetrics;
         switch (status) {
             case SUCCESS:
                 metrics = this.successMetrics;
@@ -65,22 +70,24 @@ public class ActionMetrics implements IActionMetrics {
                 metrics = this.timeoutMetrics;
                 break;
         }
-        if (currentTimestamp >= advanceSlotTimeInMillis) {
+        if (currentTimestamp >= advanceSlotTimeInMillis.get()) {
             advanceToCurrentSlot(currentTimestamp);
         }
 
-        ++metrics[slotNumber];
+        int slotNumber = this.slotNumber.get();
+        metrics.lazySet(slotNumber, metrics.get(slotNumber) + 1);
     }
 
     private void advanceToCurrentSlot(long currentTimestamp) {
+        long advanceSlotTimeInMillis = this.advanceSlotTimeInMillis.get();
         long l = (currentTimestamp - advanceSlotTimeInMillis) / 1000;
-        int slotsToAdvance =  1 + (int) l;
-        int newSlotNumber = slotsToAdvance + slotNumber;
+        int slotsToAdvance = 1 + (int) l;
+        int newSlotNumber = slotsToAdvance + slotNumber.get();
         if (newSlotNumber >= totalSlots) {
             newSlotNumber = newSlotNumber - totalSlots;
         }
-        this.slotNumber = newSlotNumber;
-        this.advanceSlotTimeInMillis = advanceSlotTimeInMillis + (1000 * slotsToAdvance);
+        this.slotNumber.lazySet(newSlotNumber);
+        this.advanceSlotTimeInMillis.lazySet(advanceSlotTimeInMillis + (1000 * slotsToAdvance));
     }
 
 }
