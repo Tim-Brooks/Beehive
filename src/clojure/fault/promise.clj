afdc34e18e693f6b1365eeff1bfd5becaccd3dba
Tim Brooks <tim.brooks@staples.com>
2014-12-14 16:24:58 -0800

    Return results in Clojurish manner

--- src/clojure/fault/promise.clj
+++ src/clojure/fault/promise.clj
@@ -9,17 +9,17 @@
     (= Status/PENDING status-enum) :pending
     (= Status/SUCCESS status-enum) :success
     (= Status/ERROR status-enum) :error
-    (= Status/TIMED_OUT status-enum) :time-out))
+    (= Status/TIMED_OUT status-enum) :timed-out))
 
 (deftype CLJResilientPromise [^ResilientPromise promise]
   IDeref
-  (deref [_] (.awaitResult promise))
+  (deref [_] (or (.awaitResult promise) (.error promise) :timed-out))
   IBlockingDeref
   (deref
     [_ timeout-ms timeout-val]
     (if (.await promise timeout-ms)
-      (.result promise)
-      timeout-val)) 
+      (or (.result promise) (.error promise) :timed-out)
+      timeout-val))
   IPending
   (isRealized [_]
     (.isDone promise))
