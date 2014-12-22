c083d9feeee0feaf67545f21da1d31c75b32261d
Tim Brooks <tim.brooks@staples.com>
2014-12-22 13:53:44 -0600

    WIP: Continue implementing ThreadPool scheduler

--- src/java/fault/scheduling/ThreadPoolVersion.java
+++ src/java/fault/scheduling/ThreadPoolVersion.java
@@ -1,6 +1,9 @@
 package fault.scheduling;
 
+import fault.ResilientPromise;
+import fault.circuit.CircuitBreaker;
 import fault.messages.ScheduleMessage;
+import fault.metrics.ActionMetrics;
 
 import java.util.concurrent.*;
 
@@ -9,18 +12,43 @@ import java.util.concurrent.*;
  */
 public class ThreadPoolVersion {
 
-    private ExecutorService service = Executors.newScheduledThreadPool(10);
-    private ScheduledExecutorService timeoutService = Executors.newScheduledThreadPool(1);
+    private final ExecutorService service = Executors.newScheduledThreadPool(10);
+    private final ExecutorService metricsService = Executors.newSingleThreadExecutor();
+    private final ScheduledExecutorService timeoutService = Executors.newScheduledThreadPool(1);
+    private final BlockingQueue<ResilientPromise<?>> metricsQueue = new LinkedBlockingQueue<>();
+
+    public ThreadPoolVersion(final ActionMetrics actionMetrics, final CircuitBreaker circuitBreaker) {
+        metricsService.submit(new Runnable() {
+            @Override
+            public void run() {
+                for (; ; ) {
+                    try {
+                        ResilientPromise<?> promise = metricsQueue.take();
+                        actionMetrics.reportActionResult(promise.getStatus());
+                        circuitBreaker.informBreakerOfResult(promise.isSuccessful());
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+
+                }
+            }
+        });
+    }
 
     public <T> void action(final ScheduleMessage<T> message) {
         final Future<Void> f = service.submit(new Callable<Void>() {
             @Override
             public Void call() {
+                ResilientPromise<T> promise = message.promise;
                 try {
                     T result = message.action.run();
-                    message.promise.deliverResult(result);
+                    if (promise.deliverResult(result)) {
+                        metricsQueue.add(promise);
+                    }
                 } catch (Exception e) {
-                    message.promise.deliverError(e);
+                    if (promise.deliverError(e)) {
+                        metricsQueue.add(promise);
+                    }
                 }
                 return null;
             }
@@ -28,11 +56,19 @@ public class ThreadPoolVersion {
         timeoutService.schedule(new Runnable() {
             @Override
             public void run() {
-                // TODO: Race with normal set.
-                message.promise.setTimedOut();
-                f.cancel(true);
+                ResilientPromise<T> promise = message.promise;
+                if (promise.setTimedOut()) {
+                    f.cancel(true);
+                    metricsQueue.add(promise);
+                }
             }
         }, message.relativeTimeout, TimeUnit.MILLISECONDS);
 
+    }
+
+    public void shutdown() {
+        service.shutdown();
+        metricsService.shutdown();
+        timeoutService.shutdown();
     }
 }
