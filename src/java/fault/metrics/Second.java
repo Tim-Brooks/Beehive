ac378008d178b88607954b341fe8abc7ac36b45b
Tim Brooks <tim.brooks@staples.com>
2015-06-01 18:42:36 -0500

    Add method to increment metrics

--- src/java/fault/metrics/Second.java
+++ src/java/fault/metrics/Second.java
@@ -13,4 +13,29 @@ public class Second {
     private final LongAdder circuitOpen = new LongAdder();
     private final LongAdder queueFull = new LongAdder();
     private final LongAdder maxConcurrencyExceeded = new LongAdder();
+
+    public void incrementMetric(Metric metric) {
+        switch (metric) {
+            case SUCCESS:
+                successes.increment();
+                break;
+            case ERROR:
+                errors.increment();
+                break;
+            case TIMEOUT:
+                timeouts.increment();
+                break;
+            case CIRCUIT_OPEN:
+                circuitOpen.increment();
+                break;
+            case QUEUE_FULL:
+                queueFull.increment();
+                break;
+            case MAX_CONCURRENCY_LEVEL_EXCEEDED:
+                maxConcurrencyExceeded.increment();
+                break;
+            default:
+                throw new RuntimeException("Unknown metric: " + metric);
+        }
+    }
 }
