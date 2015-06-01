c80d39111a15e0c11a134ae055578664604c4775
Tim Brooks <tim.brooks@staples.com>
2015-06-01 17:21:47 -0500

    update license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject fault "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :profiles {:dev {:dependencies [[junit/junit "4.11"]
                                   [org.mockito/mockito-core "1.10.8"]
                                   [org.clojure/core.async "0.1.346.0-17112a-alpha"]
