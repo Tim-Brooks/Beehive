5669b0ea2e953ce39127d2a0119efe0f0c50757a
Tim Brooks <tim.brooks@staples.com>
2015-06-01 12:02:11 -0500

    Make criterium a dev dependencie

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
   :profiles {:dev {:dependencies [[junit/junit "4.11"]
                                   [org.mockito/mockito-core "1.10.8"]
                                   [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                                  [clj-http "1.0.1"]]
+                                  [clj-http "1.0.1"]
+                                  [criterium "0.4.3"]]
                    :junit ["test/java"]
                    :java-source-paths ["src/java" "test/java"]
                    :plugins [[lein-junit "1.1.3"]]
                    :junit-formatter "brief"}}
   :javac-target "1.7"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [criterium "0.4.3"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"])
 
