947067367b7cf09e0277bab5a91ca17b369f71b4
Tim Brooks <tim.brooks@staples.com>
2015-06-04 11:50:45 -0500

    Add Pattern Interface

---
+++ src/java/fault/Pattern.java
@@ -0,0 +1,26 @@
+package fault;
+
+import fault.concurrent.ResilientFuture;
+import fault.concurrent.ResilientPromise;
+import fault.utils.ResilientPatternAction;
+
+/**
+ * Created by timbrooks on 6/4/15.
+ */
+public interface Pattern<C> {
+    long MAX_TIMEOUT_MILLIS = 1000 * 60 * 60 * 24;
+
+    <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, long millisTimeout);
+
+    <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, ResilientCallback<T> callback, long millisTimeout);
+
+    <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, ResilientPromise<T> promise, long
+            millisTimeout);
+
+    <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, ResilientPromise<T> promise,
+                                        ResilientCallback<T> callback, long millisTimeout);
+
+    <T> ResilientPromise<T> performAction(ResilientPatternAction<T, C> action);
+
+
+}
