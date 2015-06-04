3566560a1d8f49394f642d000ba3d3bb7646bdf6
Tim Brooks <tim.brooks@staples.com>
2015-06-04 12:04:32 -0500

    Fix type relationship. And have methods calls each other

--- src/java/fault/LoadBalancer.java
+++ src/java/fault/LoadBalancer.java
@@ -8,33 +8,33 @@ import fault.utils.ResilientPatternAction;
 /**
  * Created by timbrooks on 6/4/15.
  */
-public class LoadBalancer<T, C> implements Pattern {
+public class LoadBalancer<C> implements Pattern<C> {
 
     @Override
-    public ResilientFuture<T> submitAction(ResilientPatternAction action, long millisTimeout) {
-        return submitAction(action, new DefaultResilientPromise(), null, millisTimeout);
+    public <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, long millisTimeout) {
+        return submitAction(action, new DefaultResilientPromise<T>(), null, millisTimeout);
     }
 
     @Override
-    public ResilientFuture<T> submitAction(ResilientPatternAction action, ResilientCallback callback,
-                                           long millisTimeout) {
-        return null;
+    public <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, ResilientCallback<T> callback,
+                                               long millisTimeout) {
+        return submitAction(action, new DefaultResilientPromise<T>(), callback, millisTimeout);
     }
 
     @Override
-    public ResilientFuture<T> submitAction(ResilientPatternAction action, ResilientPromise promise,
-                                           long millisTimeout) {
-        return null;
+    public <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, ResilientPromise<T> promise,
+                                               long millisTimeout) {
+        return submitAction(action, promise, null, millisTimeout);
     }
 
     @Override
-    public ResilientFuture<T> submitAction(ResilientPatternAction action, ResilientPromise promise,
-                                           ResilientCallback callback, long millisTimeout) {
-        return null;
+    public <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, ResilientPromise<T> promise,
+                                               ResilientCallback<T> callback, long millisTimeout) {
+        return submitAction(action, promise, callback, millisTimeout);
     }
 
     @Override
-    public ResilientPromise<T> performAction(ResilientPatternAction action) {
+    public <T> ResilientPromise<T> performAction(ResilientPatternAction<T, C> action) {
         return null;
     }
 }
