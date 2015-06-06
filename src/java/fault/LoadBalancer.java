a12dbd18f14884eab918b35aeede19da7841ecf5
Tim Brooks <tim.brooks@staples.com>
2015-06-06 12:23:30 -0500

    Work on implementing LoadBalancer

--- src/java/fault/LoadBalancer.java
+++ src/java/fault/LoadBalancer.java
@@ -5,10 +5,25 @@ import fault.concurrent.ResilientFuture;
 import fault.concurrent.ResilientPromise;
 import fault.utils.ResilientPatternAction;
 
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by timbrooks on 6/4/15.
  */
 public class LoadBalancer<C> implements Pattern<C> {
+
+    private final ServiceExecutor[] services;
+    private final C context;
+
+    public LoadBalancer(List<ServiceExecutor> executors, C context) {
+        if (executors.size() == 0) {
+            throw new IllegalArgumentException("Cannot create LoadBalancer with 0 Executors.");
+        }
+
+        this.context = context;
+        services = (ServiceExecutor[]) executors.toArray();
+    }
 
     @Override
     public <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, long millisTimeout) {
@@ -28,13 +43,27 @@ public class LoadBalancer<C> implements Pattern<C> {
     }
 
     @Override
-    public <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, ResilientPromise<T> promise,
+    public <T> ResilientFuture<T> submitAction(final ResilientPatternAction<T, C> action, ResilientPromise<T> promise,
                                                ResilientCallback<T> callback, long millisTimeout) {
-        return submitAction(action, promise, callback, millisTimeout);
+        return services[nextService()].submitAction(new ResilientAction<T>() {
+            @Override
+            public T run() throws Exception {
+                return action.run(context);
+            }
+        }, promise, callback, millisTimeout);
     }
 
     @Override
-    public <T> ResilientPromise<T> performAction(ResilientPatternAction<T, C> action) {
-        return null;
+    public <T> ResilientPromise<T> performAction(final ResilientPatternAction<T, C> action) {
+        return services[nextService()].performAction(new ResilientAction<T>() {
+            @Override
+            public T run() throws Exception {
+                return action.run(context);
+            }
+        });
+    }
+
+    private int nextService() {
+        return new Random().nextInt(services.length) % services.length;
     }
 }
