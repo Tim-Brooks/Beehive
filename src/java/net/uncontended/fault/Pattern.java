94e2ef27e85e3745f100b2668e0314ea33ab5da2
Tim Brooks <tim.brooks@staples.com>
2015-06-19 15:31:07 -0700

    Add documentation to pattern interface

--- src/java/net/uncontended/fault/Pattern.java
+++ src/java/net/uncontended/fault/Pattern.java
@@ -7,17 +7,83 @@ import net.uncontended.fault.concurrent.ResilientPromise;
  * Created by timbrooks on 6/4/15.
  */
 public interface Pattern<C> {
+    /**
+     * Submits a {@link ResilientPatternAction} that will be ran asynchronously.
+     * The result of the action will be delivered to the future returned
+     * by this call. An attempt to cancel the action will be made if it
+     * does not complete before the timeout.
+     *
+     * @param action        the action to submit
+     * @param millisTimeout milliseconds before the action times out
+     * @return a {@link ResilientFuture} representing pending completion of the action
+     * @throws RejectedActionException if the action is rejected
+     */
     <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, long millisTimeout);
 
+    /**
+     * Submits a {@link ResilientPatternAction} that will be ran asynchronously similar to
+     * {@link #submitAction(ResilientPatternAction, long)}. However, at the completion of the
+     * task, the provided callback will be executed. The callback will be run regardless of
+     * the result of the action.
+     *
+     * @param action        the action to submit
+     * @param callback      to run on action completion
+     * @param millisTimeout milliseconds before the action times out
+     * @return a {@link ResilientFuture} representing pending completion of the action
+     * @throws RejectedActionException if the action is rejected
+     */
     <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, ResilientCallback<T> callback, long millisTimeout);
 
+    /**
+     * Submits a {@link ResilientPatternAction} that will be ran asynchronously similar to
+     * {@link #submitAction(ResilientPatternAction, long)}. However, at the completion of
+     * the task, the result will be delivered to the promise provided.
+     *
+     * @param action        the action to submit
+     * @param promise       a promise to which deliver the result
+     * @param millisTimeout milliseconds before the action times out
+     * @return a {@link ResilientFuture} representing pending completion of the action
+     * @throws RejectedActionException if the action is rejected
+     */
     <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, ResilientPromise<T> promise, long
             millisTimeout);
 
+    /**
+     * Submits a {@link ResilientPatternAction} that will be ran asynchronously similar to
+     * {@link #submitAction(ResilientPatternAction, long)}. However, at the completion
+     * of the task, the result will be delivered to the promise provided. And the provided
+     * callback will be executed.
+     *
+     * @param action        the action to submit
+     * @param promise       a promise to which deliver the result
+     * @param callback      to run on action completion
+     * @param millisTimeout milliseconds before the action times out
+     * @return a {@link ResilientFuture} representing pending completion of the action
+     * @throws RejectedActionException if the action is rejected
+     */
     <T> ResilientFuture<T> submitAction(ResilientPatternAction<T, C> action, ResilientPromise<T> promise,
                                         ResilientCallback<T> callback, long millisTimeout);
 
+    /**
+     * Performs a {@link ResilientPatternAction} that will be ran synchronously on the
+     * calling thread. However, at the completion of the task, the result will be delivered
+     * to the promise provided. And the provided callback will be executed.
+     * <p/>
+     * <p/>
+     * If the ResilientPatternAction throws a {@link ActionTimeoutException}, the result
+     * of the action will be a timeout. Any other exception and the result of the action
+     * will be an error.
+     *
+     * @param action the action to run
+     * @return a {@link ResilientPromise} representing result of the action
+     * @throws RejectedActionException if the action is rejected
+     */
     <T> ResilientPromise<T> performAction(ResilientPatternAction<T, C> action);
 
+    /**
+     * Attempts to shutdown the service. Calls made to submitAction or performAction
+     * after this call will throw a {@link RejectedActionException}. Implementations
+     * may differ on if pending or executing actions are cancelled.
+     */
     void shutdown();
 }
--- src/java/net/uncontended/fault/ServiceExecutor.java
+++ src/java/net/uncontended/fault/ServiceExecutor.java
@@ -5,10 +5,6 @@ import net.uncontended.fault.concurrent.ResilientFuture;
 import net.uncontended.fault.concurrent.ResilientPromise;
 import net.uncontended.fault.metrics.ActionMetrics;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.RejectedExecutionException;
-
 /**
  * Created by timbrooks on 12/22/14.
  */
@@ -21,7 +17,7 @@ public interface ServiceExecutor {
      * by this call. An attempt to cancel the action will be made if it
      * does not complete before the timeout.
      *
-     * @param action the action to submit
+     * @param action        the action to submit
      * @param millisTimeout milliseconds before the action times out
      * @return a {@link ResilientFuture} representing pending completion of the action
      * @throws RejectedActionException if the action is rejected
@@ -29,11 +25,13 @@ public interface ServiceExecutor {
     <T> ResilientFuture<T> submitAction(ResilientAction<T> action, long millisTimeout);
 
     /**
-     * Submits a {@link ResilientAction} that will be ran asynchronously
-     * similar to {@link #submitAction(ResilientAction, long)}. However, at the
-     * completion of the task, the provided callback will be executed.
+     * Submits a {@link ResilientAction} that will be ran asynchronously similar to
+     * {@link #submitAction(ResilientAction, long)}. However, at the completion of the task,
+     * the provided callback will be executed. The callback will be run regardless of the result
+     * of the action.
      *
-     * @param action the action to submit
+     * @param action        the action to submit
+     * @param callback      to run on action completion
      * @param millisTimeout milliseconds before the action times out
      * @return a {@link ResilientFuture} representing pending completion of the action
      * @throws RejectedActionException if the action is rejected
@@ -46,7 +44,8 @@ public interface ServiceExecutor {
      * similar to {@link #submitAction(ResilientAction, long)}. However, at the
      * completion of the task, the result will be delivered to the promise provided.
      *
-     * @param action the action to submit
+     * @param action        the action to submit
+     * @param promise       a promise to which deliver the result
      * @param millisTimeout milliseconds before the action times out
      * @return a {@link ResilientFuture} representing pending completion of the action
      * @throws RejectedActionException if the action is rejected
@@ -60,7 +59,9 @@ public interface ServiceExecutor {
      * of the task, the result will be delivered to the promise provided. And the provided
      * callback will be executed.
      *
-     * @param action the action to submit
+     * @param action        the action to submit
+     * @param promise       a promise to which deliver the result
+     * @param callback      to run on action completion
      * @param millisTimeout milliseconds before the action times out
      * @return a {@link ResilientFuture} representing pending completion of the action
      * @throws RejectedActionException if the action is rejected
@@ -72,11 +73,11 @@ public interface ServiceExecutor {
      * Performs a {@link ResilientAction} that will be ran synchronously on the calling
      * thread. However, at the completion of the task, the result will be delivered to
      * the promise provided. And the provided callback will be executed.
-     *
-     * <p>
+     * <p/>
+     * <p/>
      * If the ResilientAction throws a {@link ActionTimeoutException}, the result of
      * the action will be a timeout. Any other exception and the result of the action
-     * will be a error.
+     * will be an error.
      *
      * @param action the action to run
      * @return a {@link ResilientPromise} representing result of the action
@@ -101,8 +102,7 @@ public interface ServiceExecutor {
     /**
      * Attempts to shutdown the service. Calls made to submitAction or performAction
      * after this call will throw a {@link RejectedActionException}. Implementations
-     * may different on if pending or executing actions are cancelled.
-     *
+     * may differ on if pending or executing actions are cancelled.
      */
     void shutdown();
 }
