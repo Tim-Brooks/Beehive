b03032a4300229f30be2b9dd789b8924a3e7ce1b
Tim Brooks <tim.brooks@staples.com>
2015-06-23 12:59:02 -0700

    Rework project structure

--- project.clj
+++ project.clj
@@ -1,22 +1,15 @@
 (defproject net.uncontended/beehive "0.2.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Beehive is a Clojure facade for the Precipice library."
+  :url "https://github.com/tbrooks8/Beehive"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :profiles {:dev {:dependencies [[junit/junit "4.11"]
                                   [org.mockito/mockito-core "1.10.8"]
                                   [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                                   [clj-http "1.0.1"]
-                                  [criterium "0.4.3"]]
-                   :junit ["test/java"]
-                   :source-paths ["src/clojure" "test/clojure"]
-                   :java-source-paths ["src/java" "test/java"]
-                   :plugins [[lein-junit "1.1.3"]]
-                   :junit-formatter "brief"}}
+                                  [criterium "0.4.3"]]}}
   :javac-target "1.7"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.uncontended/Precipice "0.1.0"]]
-  :source-paths ["src/clojure"]
-  :java-source-paths ["src/java"])
+                 [net.uncontended/Precipice "0.1.0"]])
 
 
