1bad38a34f1ad03e3b3289eed65d6415a581361d
Tim Brooks <tim.brooks@staples.com>
2015-06-23 13:00:47 -0700

    Clean up project file

--- project.clj
+++ project.clj
@@ -3,12 +3,9 @@
   :url "https://github.com/tbrooks8/Beehive"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :profiles {:dev {:dependencies [[junit/junit "4.11"]
-                                  [org.mockito/mockito-core "1.10.8"]
-                                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+  :profiles {:dev {:dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]
                                   [clj-http "1.0.1"]
                                   [criterium "0.4.3"]]}}
-  :javac-target "1.7"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [net.uncontended/Precipice "0.1.0"]])
 
